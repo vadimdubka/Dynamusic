9d1e2ce8ae25a9409da86f99e29264830f1834b2
vadimdubka <vadimdubka@gmail.com>
2017-10-31 13:10:46 +0300

    Task 14 has been done.

--- src/dynamusic/SongsManager.java
+++ src/dynamusic/SongsManager.java
@@ -1,12 +1,18 @@
 package dynamusic;
 
+import atg.dtm.TransactionDemarcation;
+import atg.dtm.TransactionDemarcationException;
 import atg.nucleus.GenericService;
 import atg.repository.*;
 import atg.repository.rql.RqlStatement;
 
+import javax.transaction.SystemException;
+import javax.transaction.TransactionManager;
+
 public class SongsManager extends GenericService {
     
     private Repository repository;
+    private TransactionManager transactionManager;
     
     public SongsManager() {
         super();
@@ -20,30 +26,73 @@ public class SongsManager extends GenericService {
         this.repository = repository;
     }
     
-    public void deleteAlbumsByArtist(String artistId) throws RepositoryException {
+    public TransactionManager getTransactionManager() {
+        return transactionManager;
+    }
+    
+    public void setTransactionManager(TransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+    
+    public void deleteAlbumsByArtist(String artistId) {
         if (isLoggingDebug()) {
             logDebug("Deleting albums by artist id " + artistId);
         }
         
         MutableRepository mutRepos = (MutableRepository) repository;
         
-        RqlStatement findAlbumRQL = RqlStatement.parseRqlStatement("artist.id = ?0");
-        RepositoryView albumView = repository.getView("album");
-        
-        Object[] rqlparams = new Object[1];
-        rqlparams[0] = artistId;
-        
-        RepositoryItem[] albumList = findAlbumRQL.executeQuery(albumView, rqlparams);
-        if (isLoggingDebug())
-            logDebug("found albums for artist: " + albumList);
-        if (albumList != null) {
-            for (RepositoryItem album : albumList) {
-                String albumId = (String) album.getPropertyValue("id");
-                mutRepos.removeItem(album.getRepositoryId(), "album");
+        TransactionDemarcation transacDemar = new TransactionDemarcation();
+        try {
+            transacDemar.begin(transactionManager, transacDemar.REQUIRED);
+            
+            try {
+                RqlStatement findAlbumRQL = RqlStatement.parseRqlStatement("artist.id = ?0");
+                RepositoryView albumView = repository.getView("album");
+                
+                Object[] rqlparams = new Object[1];
+                rqlparams[0] = artistId;
+                
+                RepositoryItem[] albumList = findAlbumRQL.executeQuery(albumView, rqlparams);
                 
                 if (isLoggingDebug()) {
-                    logDebug("album id=" + albumId + " is deleted.");
+                    logDebug("found albums for artist: " + albumList);
                 }
+                
+                
+                if (albumList != null) {
+                    for (RepositoryItem album : albumList) {
+                        String albumId = (String) album.getPropertyValue("id");
+                        
+                        mutRepos.removeItem(album.getRepositoryId(), "album");
+                        
+                        
+                        if (isLoggingDebug()) {
+                            logDebug("album id=" + albumId + " is deleted.");
+                        }
+                    }
+                }
+                
+            } catch (RepositoryException e) {
+                if (isLoggingError()) {
+                    logError("Unable to delete album", e);
+                }
+                try {
+                    transactionManager.setRollbackOnly();
+                } catch (SystemException e1) {
+                    if (isLoggingError())
+                        logError("Rollback failed.", e);
+                    
+                }
+            }
+        } catch (TransactionDemarcationException e) {
+            if (isLoggingError())
+                logError("Creating transaction demarcation BEGIN failed, no albums deleted", e);
+        } finally {
+            try {
+                transacDemar.end();
+            } catch (TransactionDemarcationException e) {
+                if (isLoggingError())
+                    logError("Creating transaction demarcation END failed, no albums deleted", e);
             }
         }
     }
